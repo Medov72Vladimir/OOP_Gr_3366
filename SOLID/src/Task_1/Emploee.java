0d26fc6d4f1a68f7e3246997e932f1dfee34bd1b
Medov72Vladimir <119006643+Medov72Vladimir@users.noreply.github.com>
2023-05-26 19:06:43 +0300

    Семинар 6/Task_1

---
+++ SOLID/src/Task_1/Emploee.java
@@ -0,0 +1,17 @@
+package Task_1;
+
+import java.util.Date;
+
+public class Emploee {
+    private String name;
+    private Date dob;
+
+    public Emploee(String name, Date dob) {
+        this.name = name;
+        this.dob = dob;
+    }
+
+    public String getEmpInfo() {
+        return "name - " + name + " , dob - " + dob.toString();
+    }
+}
